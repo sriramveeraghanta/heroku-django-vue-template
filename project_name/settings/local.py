c299d2843c3b59c693b3b4b219d3bed5481f184d
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2017-11-28 22:02:30 +0530

    Custom Heruko django template

---
+++ project_name/settings/local.py
@@ -0,0 +1,37 @@
+"""Development settings and globals."""
+
+from __future__ import absolute_import
+
+from .common import * # noqa
+
+DEBUG = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "{{project_name}}",
+        'USER': "",
+        'PASSWORD': "",
+        'HOST': "",
+    }
+}
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+
+INSTALLED_APPS += (
+)
+
+MIDDLEWARE_CLASSES += (
+)
+
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+INTERNAL_IPS = ('127.0.0.1',)
